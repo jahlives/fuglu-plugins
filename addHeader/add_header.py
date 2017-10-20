599c3ea54e8f205ede814c2d98682f18f7413b28
Tobi Szyndler <ts@apexis.ch>
2017-10-20 22:43:04 +0200

    addheader feature moved to own plugin to keep changes to upstream code to a minimum

--- addHeader/add_header.py
+++ addHeader/add_header.py
@@ -71,3 +71,78 @@ class AttachmentAddHeader(ScannerPlugin):
         return DUNNO
 #        return string_to_actioncode(self.config.get('URIExtractPlugin', 'action'), self.config), apply_template(
 #            self.config.get('URIExtractPlugin', 'message'), suspect, dict(domain=urls[0], blacklist='tbd'))
+
+class ClamAddHeader(ScannerPlugin):
+    def __init__(self, config, section=None):
+        ScannerPlugin.__init__(self, config, section)
+        self.logger = logging.getLogger('fuglu.plugin.DomainAction')
+
+        self.requiredvars = {
+             'blockedaddheader': {
+                'default': '0',
+                'description': 'if set to non zero value a header will be added for blocked files and the message will be accepted\n1:\tonly filename appended as header\n2:\tfilename and details will be added as header\nany other string value will be added as-it-is to header',
+            }
+        }
+
+    def examine(self, suspect):
+        urls = suspect.get_tag('clam.virus', defaultvalue=[])
+        add_links = self.config.get('ClamavPlugin', 'addheaderinfected')
+        if len(urls) == 0 or add_links == '0':
+            return DUNNO
+        elif add_links == '1':
+            suspect.add_header('X-Fuglu-ClamAV', str(urls), immediate=True)
+        elif add_links == '2':
+            suspect.add_header('X-Fuglu-ClamAV', str(urls), immediate=True)
+        else:
+            suspect.add_header('X-Fuglu-ClamAV', str(add_links), immediate=True)
+        return DUNNO
+
+class FprotAddHeader(ScannerPlugin):
+    def __init__(self, config, section=None):
+        ScannerPlugin.__init__(self, config, section)
+        self.logger = logging.getLogger('fuglu.plugin.DomainAction')
+
+        self.requiredvars = {
+             'blockedaddheader': {
+                'default': '0',
+                'description': 'if set to non zero value a header will be added for blocked files and the message will be accepted\n1:\tonly filename appended as header\n2:\tfilename and details will be added as header\nany other string value will be added as-it-is to header',
+            }
+        }
+
+    def examine(self, suspect):
+        urls = suspect.get_tag('fprot.virus', defaultvalue=[])
+        add_links = self.config.get('FprotPlugin', 'addheaderinfected')
+        if len(urls) == 0 or add_links == '0':
+            return DUNNO
+        elif add_links == '1':
+            suspect.add_header('X-Fuglu-Fprot', str(urls), immediate=True)
+        elif add_links == '2':
+            suspect.add_header('X-Fuglu-Fprot', str(urls), immediate=True)
+        else:
+            suspect.add_header('X-Fuglu-Fprot', str(add_links), immediate=True)
+        return DUNNO
+
+class SsspAddHeader(ScannerPlugin):
+    def __init__(self, config, section=None):
+        ScannerPlugin.__init__(self, config, section)
+        self.logger = logging.getLogger('fuglu.plugin.DomainAction')
+
+        self.requiredvars = {
+             'blockedaddheader': {
+                'default': '0',
+                'description': 'if set to non zero value a header will be added for blocked files and the message will be accepted\n1:\tonly filename appended as header\n2:\tfilename and details will be added as header\nany other string value will be added as-it-is to header',
+            }
+        }
+
+    def examine(self, suspect):
+        urls = suspect.get_tag('sssp.virus', defaultvalue=[])
+        add_links = self.config.get('SSSPPlugin', 'addheaderinfected')
+        if len(urls) == 0 or add_links == '0':
+            return DUNNO
+        elif add_links == '1':
+            suspect.add_header('X-Fuglu-Sophos', str(urls), immediate=True)
+        elif add_links == '2':
+            suspect.add_header('X-Fuglu-Sohpos', str(urls), immediate=True)
+        else:
+            suspect.add_header('X-Fuglu-Sophos', str(add_links), immediate=True)
+        return DUNNO
